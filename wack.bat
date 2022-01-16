5d67c76c22ed22dd588bb7ea93cf8a71edf733cd
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2022-01-16 12:01:41 +0000

    Update wack.bat

--- wack.bat
+++ wack.bat
@@ -1,6 +1,11 @@
-echo off
+@echo off
+:: Subroutine of gobackup.bat
+:: This subroutine tidys up a bit. Keeping things secure
+:: by <https://github.com/GrantAccess/Janis-Verona/> (CC)
+:: the author has not changed this document since Sunday 16 Jan 2022
 
-:: this routine tidys up a bit.
+:: This routine interacts with the user
+:: to make this automatic, comment out the pause lines, use the /q (quite) switch with RMDIR
 
 :: where?
 IF EXIST %ARCHIVE% GOTO 7BACK
@@ -17,6 +22,9 @@ PAUSE
 RMDIR /S %ARCHIVE%
 GOTO DONE
 
+:: Clear the browser history
+
+:: empty the recycle bin
 :MTBIN
 :: rd /s /q C:\$Recycle.Bin
 

218a1d51e649f257475b6a2fdfa9406f57fd2d46
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2022-01-16 11:49:21 +0000

    Update nowdate.bat

--- nowdate.bat
+++ nowdate.bat
@@ -1,5 +1,8 @@
-echo off
-:: This subroutine gets the system date and time to use as a unique filename
+@echo off
+:: Subroutine of gobackup.bat
+:: This subroutine gets the system date and time to use as a unique filename for archives
+:: by <https://github.com/GrantAccess/Janis-Verona/> (CC)
+:: the author has not changed this document since Sunday 16 Jan 2022
 
 set CUR_YYYY=%date:~6,4%
 set CUR_MM=%date:~3,2%
@@ -12,5 +15,7 @@ set CUR_SS=%time:~6,2%
 set CUR_MS=%time:~9,2%
 
 set SUBFILENAME=%CUR_YYYY%-%CUR_MM%-%CUR_DD%-%CUR_HH%-%CUR_NN%
+:: do not make backups to your normal work folder, or you will be making backups of backups!
+:: this is where the backups are kept.
 mkdir %USERPROFILE%\archive\backup\%SUBFILENAME%-%USERNAME%
 set ARCHIVE=%USERPROFILE%\archive\backup\%SUBFILENAME%-%USERNAME%

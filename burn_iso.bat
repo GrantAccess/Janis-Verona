a23e5350356cf9ff949dd0632e45d6e726829ef8
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2022-01-31 15:10:46 +0000

    cd shutdown

--- burn_iso.bat
+++ burn_iso.bat
@@ -1,6 +1,12 @@
 
+:: change to the download dir
+
+
 :: from an idea at <https://www.youtube.com/watch?v=Di2nLa8mYaQ&t=16s>
 certutil -urlcache -f -split https://mirror.dogado.de/linuxmint-cd/stable/20.3/linuxmint-20.3-cinnamon-64bit.iso
 
 :: from an idea at <https://www.youtube.com/watch?v=kK6sCxYq0ZY>
 isoburn.exe /q "C:\images\linuxmint-20.3-cinnamon-64bit.iso"
+
+:: restart the PC
+:: shutdown -r

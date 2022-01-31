6f7e17b277b7bac96eef28bc68e85617d9daf3ab
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2022-01-31 14:57:18 +0000

    get linux mint

--- burn_iso.bat
+++ burn_iso.bat
@@ -1,3 +1,6 @@
-:: from an idea at <https://www.youtube.com/watch?v=kK6sCxYq0ZY>
 
-isoburn.exe /q "C:\images\my new linux.iso"
+:: from an idea at <https://www.youtube.com/watch?v=Di2nLa8mYaQ&t=16s>
+certutil -urlcache -f -split https://mirror.dogado.de/linuxmint-cd/stable/20.3/linuxmint-20.3-cinnamon-64bit.iso
+
+:: from an idea at <https://www.youtube.com/watch?v=kK6sCxYq0ZY>
+isoburn.exe /q "C:\images\linuxmint-20.3-cinnamon-64bit.iso"

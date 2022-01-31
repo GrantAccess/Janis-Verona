4198d7f94fe0799755a57a54d2f2bb4a2fdb65e8
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2022-01-31 16:49:19 +0000

    choose iso

--- burn_iso.bat
+++ burn_iso.bat
@@ -1,6 +1,8 @@
 
 :: change to the download dir
 
+:: If memory is less than 1Gb use lubuntu 32bit
+:: otherwise use linuxmint
 
 :: from an idea at <https://www.youtube.com/watch?v=Di2nLa8mYaQ&t=16s>
 certutil -urlcache -f -split https://mirror.dogado.de/linuxmint-cd/stable/20.3/linuxmint-20.3-cinnamon-64bit.iso
